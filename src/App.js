9dd4b1b8d75a93db4c9bfc261bf5724617e1a42c
alexander <zeimedee@gmail.com>
2021-03-16 12:25:03 +0000

    :rocket: react chat app ref

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './App.css';
 import {ChatContext} from './context/chatContext'
 import ChatWindow from './components/chatwindow'
-import TextBubble from './components/textBubble';
-import TextBox from './components/textbox';
 
 function App() {
   const [text,setText] = useState('');
--- src/components/textbox.js
+++ src/components/textbox.js
@@ -1,8 +1,9 @@
-import React,{useContext} from 'react'
+import React,{useContext,useRef,useEffect} from 'react'
 import {ChatContext} from '../context/chatContext'
 
 function TextBox() {
     const {text,setText,setMsgs,msgs} = useContext(ChatContext);
+    const chat = useRef();
 
     const handelClick =(e)=>{
         e.preventDefault();
@@ -11,12 +12,19 @@ function TextBox() {
         }else{
             setMsgs(msgs => msgs.concat(text));
             setText('');
+            chat.current.focus();
         }
         
     }
+
+    useEffect(() => {
+        chat.current.focus();
+    }, [])
+
     return (
         <div className='textbox'>
             <textarea 
+            ref={chat}
             type='text'
             value={text} 
             onChange={ (e)=>{setText(e.target.value)} }
